7f6a32425f1b2fa6ab40d913002ffdbd3a9589f5
Thang Nguyen <thang.nguyencao@gmail.com>
2017-06-13 11:11:59 +0700

    [fix_npe] Fixed NPE while reading data

--- src/main/java/org/embulk/output/mailchimp/MailChimpAbstractRecordBuffer.java
+++ src/main/java/org/embulk/output/mailchimp/MailChimpAbstractRecordBuffer.java
@@ -267,7 +267,8 @@ public abstract class MailChimpAbstractRecordBuffer
                 property.put("status", task.getDoubleOptIn() ? PENDING.getType() : SUBSCRIBED.getType());
                 ObjectNode mergeFields = JsonNodeFactory.instance.objectNode();
                 for (String allowColumn : allowColumns.keySet()) {
-                    String value = input.findValue(allowColumns.get(allowColumn)).asText();
+                    JsonNode field = input.findValue(allowColumns.get(allowColumn));
+                    String value = field != null ? field.asText() : "";
                     mergeFields.put(allowColumn, value);
                 }
 
@@ -275,7 +276,8 @@ public abstract class MailChimpAbstractRecordBuffer
                 if (task.getMergeFields().isPresent() && !task.getMergeFields().get().isEmpty()) {
                     for (final Column column : schema.getColumns()) {
                         if (!"".equals(containsCaseInsensitive(column.getName(), task.getMergeFields().get()))) {
-                            String value = input.findValue(column.getName()).asText();
+                            JsonNode field = input.findValue(column.getName());
+                            String value = field != null ? field.asText() : "";
 
                             // Try to convert to Json from string with the merge field's type is address
                             if (availableMergeFields.get(column.getName()).getType()
